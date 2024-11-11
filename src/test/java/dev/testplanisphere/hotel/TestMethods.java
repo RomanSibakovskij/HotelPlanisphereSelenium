6506ba55cbc56ac0a29188cb4e443e0b72540b83
RomanSibakovskij <j4023958@gmail.com>
2024-11-11 16:26:13 +0200

    Add empty TestMethods sheet

---
+++ src/test/java/dev/testplanisphere/hotel/TestMethods.java
@@ -0,0 +1,11 @@
+package dev.testplanisphere.hotel;
+
+import org.slf4j.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMethods extends BaseTest{
+
+    protected static final Logger logger = LoggerFactory.getLogger(TestMethods.class);
+
+
+}
