cd52cb3293eaee47f62b750eb568f34c9917c3fb
RomanSibakovskij <j4023958@gmail.com>
2024-11-11 17:16:45 +0200

    Add empty SignUpPage sheet

---
+++ src/main/java/dev/testplanisphere/hotel/SignUpPage.java
@@ -0,0 +1,15 @@
+package dev.testplanisphere.hotel;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class SignUpPage extends BasePage{
+
+
+    public SignUpPage(WebDriver driver) {super(driver);}
+
+
+}
