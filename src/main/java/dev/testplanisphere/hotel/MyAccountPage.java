aa8cbf5e9714da31d53db1b492350dccd9622b15
RomanSibakovskij <j4023958@gmail.com>
2024-11-12 06:33:52 +0200

    Add empty MyAccountPage sheet

---
+++ src/main/java/dev/testplanisphere/hotel/MyAccountPage.java
@@ -0,0 +1,13 @@
+package dev.testplanisphere.hotel;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class MyAccountPage extends BasePage{
+
+    public MyAccountPage(WebDriver driver) {super(driver);}
+
+}
