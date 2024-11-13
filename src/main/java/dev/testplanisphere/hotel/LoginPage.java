68989baa3b0c88869348ffa2eaa6d43fa10dee66
RomanSibakovskij <j4023958@gmail.com>
2024-11-13 07:26:11 +0200

    Add empty LoginPage sheet

---
+++ src/main/java/dev/testplanisphere/hotel/LoginPage.java
@@ -0,0 +1,13 @@
+package dev.testplanisphere.hotel;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class LoginPage extends BasePage{
+
+    public LoginPage(WebDriver driver) {super(driver);}
+    
+}
