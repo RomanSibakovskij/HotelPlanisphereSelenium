07bdc1a18699ed98aa9666f5784aed42e23886d7
RomanSibakovskij <j4023958@gmail.com>
2024-11-14 05:53:03 +0200

    Add empty ReserveDashboardPage sheet

---
+++ src/main/java/dev/testplanisphere/hotel/ReserveDashboardPage.java
@@ -0,0 +1,16 @@
+package dev.testplanisphere.hotel;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.List;
+
+public class ReserveDashboardPage extends BasePage{
+
+
+    public ReserveDashboardPage(WebDriver driver) {super(driver);}
+
+    
+}
