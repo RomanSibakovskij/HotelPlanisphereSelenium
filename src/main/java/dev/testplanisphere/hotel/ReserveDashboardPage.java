7cfbf5b215bfd7920d86fd21a7a6011c9f6f3bf2
RomanSibakovskij <j4023958@gmail.com>
2024-11-14 06:05:10 +0200

    Add web elements for ReserveDashboardPage

--- src/main/java/dev/testplanisphere/hotel/ReserveDashboardPage.java
+++ src/main/java/dev/testplanisphere/hotel/ReserveDashboardPage.java
@@ -9,8 +9,54 @@ import java.util.List;
 
 public class ReserveDashboardPage extends BasePage{
 
+    //reserve dashboard page web elements
+    @FindBy(xpath = "//h2")
+    private WebElement reserveDashboardPageTitle;
+    //list elements
+    @FindBy(xpath = "//div[@class='card-header']")
+    private List<WebElement> cardHeaderElements;
+    @FindBy(xpath = "//h5[@class='card-title']")
+    private List<WebElement> cardTitleElements;
+    @FindBy(xpath = "//ul[@class='list-unstyled']")
+    private List<WebElement> cardDescriptionElements;
+    private List<WebElement> cardReserveRoomButtonElements = driver.findElements(By.xpath("//a[@class='btn btn-primary']"));
 
     public ReserveDashboardPage(WebDriver driver) {super(driver);}
 
-    
+    //reserve page dashboard web element assert methods
+    public boolean isReserveDashboardPageTitleDisplayed(){return reserveDashboardPageTitle.isDisplayed();}
+
+    public boolean isCardHeaderDisplayed() {
+        for (WebElement element : cardHeaderElements) {
+            if (!element.isDisplayed()) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean isCardTitleDisplayed() {
+        for (WebElement element : cardTitleElements) {
+            if (!element.isDisplayed()) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean isCardDescriptionDisplayed() {
+        for (WebElement element : cardDescriptionElements) {
+            if (!element.isDisplayed()) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean isCardReserveButtonDisplayed() {
+        for (WebElement element : cardReserveRoomButtonElements) {
+            if (!element.isDisplayed()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
